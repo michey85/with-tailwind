6e74bd2a02621cec22b1e30194d308b79bc8834b
Mikhail Nepomniashchii <michey85@gmail.com>
2024-05-26 22:24:24 +0200

    final css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundPosition: {
+        'right-top': 'right 2rem top',
+      },
+      colors(theme) {
+        return {
+          primary: {
+            DEFAULT: 'hsl(263, 55%, 52%)',
+            dark: 'hsl(217, 19%, 35%)',
+            blackish: 'hsl(219, 29%, 14%)',
+          },
+          gray: {
+            ...theme.colors.gray,
+            100: 'hsl(0, 0%, 81%)',
+            200: 'hsl(210, 46%, 95%)',
+          },
+        };
+      },
+    },
   },
   plugins: [],
 };
